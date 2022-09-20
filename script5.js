373ad9ece21fdca822e289d56670d02ab724589e
niko254 <110354934+niko254@users.noreply.github.com>
2022-09-20 23:08:07 +0900

    Create script5.js

---
+++ script5.js
@@ -0,0 +1,76 @@
+const quiz = [
+  {
+    question :'鎌倉幕府が滅びる',
+    correct : '1333'
+  },
+  {
+    question: '建武の新政',
+    correct: '1334'
+  },
+  {
+    question: '南北朝の対立が始まる',
+    correct: '1336'
+  },
+  {
+    question :'足利尊氏が征夷大将軍となる',
+    correct : '1338'
+  },
+  {
+    question:'足利義満が室町に幕府を移す',
+    correct: '1378'
+  },
+  {
+    question:'南北朝が統一される',
+    correct: '1392'
+  },
+];
+quiz.sort(() => 0.5 - Math.random());
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
+
+const answerInput = document.getElementById('answer-input');
+const submitButton = document.getElementById('submit-button');
+
+// クイズの問題文を表示
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
+  answerInput.value = '';
+};
+
+setupQuiz();
+
+const clickHandler = () => {
+  quiz[quizIndex].input = answerInput.value;
+  if (quiz[quizIndex].correct === answerInput.value) {
+    window.alert('正解');
+    score++;
+  } else {
+    window.alert('不正解');
+  }
+  quizIndex++; // 次の問題へ
+  if (quizIndex < quizLength) {
+    setupQuiz();
+  } else {
+    window.alert('終了 正解数は' + score + '/' + quizLength + 'です');
+    showAllAnswers();
+  }
+};
+
+// ボタンクリックで正誤判定
+submitButton.addEventListener('click', () => {
+  clickHandler();
+});
+
+const showAllAnswers = () => {
+  const container = document.getElementById('all-answers-container');
+  container.style.display = 'block';
+  
+  const tbody = document.getElementById('all-answers-tbody');
+  for (const q of quiz) {
+    const row = document.createElement('tr');
+    row.innerHTML = `<td>${q.question}</td><td>${q.input}</td><td>${q.correct}</td><td>${q.input === q.correct ? '○' : '×'}</td>`;
+    tbody.appendChild(row);
+  }
+};
