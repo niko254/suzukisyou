41e200b67b81205167989d1dfac8b92266fe02a6
niko254 <110354934+niko254@users.noreply.github.com>
2022-09-20 23:09:54 +0900

    Create script6.js

---
+++ script6.js
@@ -0,0 +1,72 @@
+const quiz = [
+  {
+    question :'勘合貿易が始まる',
+    correct : '1404'
+  },
+  {
+    question: '近畿地方で正長の土一揆がおこる',
+    correct: '1428'
+  },
+  {
+    question: '尚氏が琉球統一',
+    correct: '1429'
+  },
+  {
+    question :'応仁の乱',
+    correct : '1467'
+  },
+  {
+    question:'応仁の乱終わり',
+    correct: '1477'
+  },
+];
+quiz.sort(() => 0.5 - Math.random());
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
+
+const answerInput = document.getElementById('answer-input');
+const submitButton = document.getElementById('submit-button');
+
+// クイズの問題文を表示
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
+  answerInput.value = '';
+};
+
+setupQuiz();
+
+const clickHandler = () => {
+  quiz[quizIndex].input = answerInput.value;
+  if (quiz[quizIndex].correct === answerInput.value) {
+    window.alert('正解');
+    score++;
+  } else {
+    window.alert('不正解');
+  }
+  quizIndex++; // 次の問題へ
+  if (quizIndex < quizLength) {
+    setupQuiz();
+  } else {
+    window.alert('終了 正解数は' + score + '/' + quizLength + 'です');
+    showAllAnswers();
+  }
+};
+
+// ボタンクリックで正誤判定
+submitButton.addEventListener('click', () => {
+  clickHandler();
+});
+
+const showAllAnswers = () => {
+  const container = document.getElementById('all-answers-container');
+  container.style.display = 'block';
+  
+  const tbody = document.getElementById('all-answers-tbody');
+  for (const q of quiz) {
+    const row = document.createElement('tr');
+    row.innerHTML = `<td>${q.question}</td><td>${q.input}</td><td>${q.correct}</td><td>${q.input === q.correct ? '○' : '×'}</td>`;
+    tbody.appendChild(row);
+  }
+};
